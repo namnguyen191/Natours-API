bc6f4db57f482a86138f9527b66a2246b605c21e
Nam Nguyen <hoangnamnguyen191@gmail.com>
2020-05-17 20:20:01 -0400

    Change DB config env

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ process.on('uncaughtException', (err) => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-);
-
-mongoose.connect(DB, {
+mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
   useCreateIndex: true,
   // useFindAndModify: false,
