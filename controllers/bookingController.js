94a6c460f0185223d0be63a3abf9f040ed090b98
Nam Nguyen <hoangnamnguyen191@gmail.com>
2020-05-18 14:35:06 -0400

    Attemp to fix checkout 3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -18,7 +18,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         success_url: `${req.protocol}://${req.get('host')}/my-tours`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
-        client_reference_id: req.params.tourId,
+        client_reference_id: req.params.tourID,
         line_items: [
             {
                 name: `${tour.name} Tour`,
@@ -51,14 +51,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = catchAsync(async (session) => {
-    console.log(session);
-    
     const tour = session.client_reference_id;
     const user = (await User.findOne({
         email: session.customer_email
     })).id;
 
-    const price = session.line_items[0].amount / 100;
+    const price = session.display_items[0].amount / 100;
 
     await Booking.create({ tour, user, price });
 });
